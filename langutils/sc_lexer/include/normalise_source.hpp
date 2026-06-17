9d10bc5209a8509f6a6d732bdf1185369280aabb
JordanHendersonMusic <j.henderson.music@outlook.com>
2026-06-17 11:52:44 +0100

    remove const&& conversion to satisfy gcc warning

--- langutils/sc_lexer/include/normalise_source.hpp
+++ langutils/sc_lexer/include/normalise_source.hpp
@@ -16,7 +16,8 @@ struct NormalisedSource {
     // Turns into a string or immutable reference to one depending on the qualifier
     [[nodiscard]] explicit operator const std::string&() const& { return s; }
     [[nodiscard]] explicit operator std::string() && { return std::move(s); }
-    [[nodiscard]] explicit operator std::string() const&& { return s; }
+    // Deleted to silence strict conversion warnings. const&& decay to const&.
+    [[nodiscard]] explicit operator std::string() const&& = delete;
 
 private:
     std::string s {};
